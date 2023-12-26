608ee91ccc7602bb85dd4e05e1efd9d422b1eb66
Amir11f <zohrabiamirhossain@gmail.com>
2023-12-26 17:46:06 +0330

    ui update

--- src/components/Home/header.jsx
+++ src/components/Home/header.jsx
@@ -1,13 +1,28 @@
-
+import rafiki from '../../../public/rafiki.png'
 export default function HomeHeader() {
     return(
         <>
-            <div className="">
-                <div className="">
+            <div className="flex flex-row">
+                <div id='homeComponentHeaderPng' className="" >
                     
                 </div>
-                <div>
-
+                <div className='flex flex-col'>
+                    <h2 className=' text-[#3574F0] font-[600] text-[28px] leading-[42px]'>
+                            .طراحی سایت حرفه‌ای، کلید موفقیت آنلاین شماست، با ما به دنیای دیجیتال خوش آمدید
+                    </h2>
+                    <p className='text-[18px] font-[600] w-[713px] h-[135px]'>
+                        ماوریکس یک استارتاپ نوآور و پیشرو است که به طراحی و توسعه اپلیکیشن‌های موبایل و وب، طراحی سایت، و راهکارهای دیجیتال متمرکز است. ما با تیمی از متخصصان متعهد و خلاق، به دنبال ارائه راهکارهایی منحصر به فرد و برتر برای مشتریانمان هستیم. اعتقاد ما این است که ارتباط مستقیم با مشتریان و تمرکز بر ارزش‌های آنها، باعث می‌شود تا محصولات و خدمات ما بهترین تجربه را برای آنها فراهم کند.
+                    </p>
+                    <div className='h-[20rem] flex flex-row'>
+                        <div className='Rectangle59492 flex item-center content-center'>
+                            <p className='w-[100%] self-center justify-self-center'>
+                            نیاز به مشاوره دارم
+                            </p>
+                        </div>
+                        <div className='Rectangle59491'>
+                            طراحی سایت  
+                        </div>
+                    </div>
                 </div>
 
             </div>

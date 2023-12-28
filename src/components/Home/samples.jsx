3d8974d0ae78b5e9178d8e7bd281b58bdd441786
Amir11f <zohrabiamirhossain@gmail.com>
2023-12-28 21:15:33 +0330

    update

--- src/components/Home/samples.jsx
+++ src/components/Home/samples.jsx
@@ -14,9 +14,12 @@ export default function Samples (){
                     </div>
                 </div>
                 <div className="flex flex-row w-[100%] itme-center justify-center">
-                    <div className="homeDetailBox flex justify-center">
-                        <div className="homeDetailBoxPicture"></div>
-                        <p></p>
+                    <div className="homeDetailBox flex flex-col item-center">
+                        <div className="homeDetailBoxPicture flex self-center"></div>
+                        <div className=" w-[100%] flex flex-row w-[100%] justify-end mr-[11px]">
+                            <p className="homeDetailBoxTitleText text-right">ساخت ربات تلگرامی</p>
+                            <p className=" homeDetailBoxTitleText text-right mr-[11px]">: پروژه</p>
+                        </div>
                     </div>
                     {/* <div className="homeDetailBox"></div>
                     <div className="homeDetailBox"></div> */}

3bad4b6533f3c09c80d926a2618855ae9a554ab0
Amir11f <zohrabiamirhossain@gmail.com>
2024-01-01 17:05:47 +0330

    ui update

--- src/components/Home/clientsOpinion.jsx
+++ src/components/Home/clientsOpinion.jsx
@@ -6,7 +6,10 @@ export function ClientsOpinion(){
 
     return(
         <>
-        
+            <div className="flex flex-row ">
+                <div className="leftOpinionBox"></div>
+                <div className="rightOpinionBox"></div>
+            </div>
         </>
     )
 }

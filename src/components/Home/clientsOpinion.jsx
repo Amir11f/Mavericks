c9a45d5b7c3607394f4770bae27137846de34588
Amir11f <zohrabiamirhossain@gmail.com>
2024-01-02 12:26:55 +0330

    solving clientsopinion bug

--- src/components/Home/clientsOpinion.jsx
+++ src/components/Home/clientsOpinion.jsx
@@ -6,9 +6,13 @@ export function ClientsOpinion(){
 
     return(
         <>
-            <div className="flex flex-row ">
-                <div className="leftOpinionBox"></div>
-                <div className="rightOpinionBox"></div>
+            <div className="flex flex-col ">
+                <img src="/opinionWhiteRectangle.png" className="leftOpinionBox" alt="" >
+                    <div>
+                        
+                    </div>
+                </img>
+                <img src="/opinionBlueRectangle.png" className="rightOpinionBox flex self-end -top-[13.2rem]" alt=""></img>
             </div>
         </>
     )

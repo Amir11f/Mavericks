0720a390bc51cc835e9224e33362510652a4a914
Amir11f <zohrabiamirhossain@gmail.com>
2024-01-02 13:32:38 +0330

    ui update

--- src/components/Home/clientsOpinion.jsx
+++ src/components/Home/clientsOpinion.jsx
@@ -7,12 +7,25 @@ export function ClientsOpinion(){
     return(
         <>
             <div className="flex flex-col ">
-                <img src="/opinionWhiteRectangle.png" className="leftOpinionBox" alt="" >
-                    <div>
-                        
+                <div className="leftOpinionBox"  >
+                    <div className="flex flex-row ">
+                        <div className="clientOpinionBox w-[286px] h-[114px] bg-[#FFFFFF] ml-[149px] mt-[42px] rounded-[5px]">
+                            <div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+                        <div className="clientOpinionBox w-[286px] h-[114px] bg-[#FFFFFF] mt-[42px] ml-[20px] rounded-[5px]">
+                            <div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
                     </div>
-                </img>
-                <img src="/opinionBlueRectangle.png" className="rightOpinionBox flex self-end -top-[13.2rem]" alt=""></img>
+                </div>
+                <div className="rightOpinionBox flex self-end -top-[13.2rem]" ></div>
             </div>
         </>
     )

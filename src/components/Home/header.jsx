572916dfb73f59a8a0cfffc036f8344637c3399f
Amir11f <zohrabiamirhossain@gmail.com>
2024-01-02 17:08:07 +0330

    ui update

--- src/components/Home/header.jsx
+++ src/components/Home/header.jsx
@@ -3,14 +3,14 @@ export default function HomeHeader() {
     return(
         <>
             <div className="flex flex-row w-[100%] h-[630px] reletive gap-[115px] top-[118px] right-[70px] left-[44px] justify-center">
-                <div id='polygonInHomeHeader'></div>
+                <div id='polygonInHomeHeader top-[196px]'></div>
                 <div id='homeComponentHeaderPng'></div>
                 <div className='flex flex-col mr-[70px]  mt-[222px]'>
                     <p id="fontfamily" className=' text-[#3574F0] font-IRANSansX font-[600] text-[28px] text-right -[42px] w-[699px] h-[84px] flex self-end'>
                             طراحی سایت حرفه‌ای، کلید موفقیت آنلاین شماست، با ما به
                              دنیای دیجیتال خوش آمدید
                     </p>
-                    <p id="fontfamily" className='text-[18px] text-right font-[600] w-[713px] h-[135px] mt-[12px] flex self-end' id='homeHederExplaination'>
+                    <p className='text-[18px] fontfamily text-right font-[600] w-[713px] h-[135px] mt-[12px] flex self-end' id='homeHederExplaination'>
                         ماوریکس یک استارتاپ نوآور و پیشرو است که به طراحی و توسعه اپلیکیشن‌های موبایل و وب، طراحی سایت، و راهکارهای دیجیتال متمرکز است. ما با تیمی از متخصصان متعهد و خلاق، به دنبال ارائه راهکارهایی منحصر به فرد و برتر برای مشتریانمان هستیم. اعتقاد ما این است که ارتباط مستقیم با مشتریان و تمرکز بر ارزش‌های آنها، باعث می‌شود تا محصولات و خدمات ما بهترین تجربه را برای آنها فراهم کند.
                     </p>
                     <div className='h-[20rem] flex flex-row gap-[43px] mt-[57px] ml-[70px]'>

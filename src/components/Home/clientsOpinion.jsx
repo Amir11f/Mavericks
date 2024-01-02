424a8da0f9a9982b1fd9aecfcc71f4617c4bd1e6
Amir11f <zohrabiamirhossain@gmail.com>
2024-01-02 19:38:00 +0330

    ui update

--- src/components/Home/clientsOpinion.jsx
+++ src/components/Home/clientsOpinion.jsx
@@ -8,10 +8,16 @@ export function ClientsOpinion(){
         <>  
             <div className="polygonInHomeClientOpinion"></div>
             <div className="polygonInHomeClientLeft"></div>
+            <div className="relative -bottom-[20rem] flex flex-row left-[5rem] top-[9rem]">
+                    <img src="/Group1.png" className="group1 absolute  left-[3rem]" alt="" />
+                    <img src="/Group1.png" className="group1 absolute left-[22rem] -top-[3rem]" alt="" />
+                    <img src="/Group1.png" className="group1 absolute left-[38rem]" alt="" />
+            </div>
             <div className="flex flex-col mt-[5rem] ">
-                <div className="leftOpinionBox"  >
+                <div className="leftOpinionBox flex flex-row"  >
+                    <img src="/PolygonRight.png" alt="" className="Polygons" />
                     <div className="flex flex-row ">
-                        <div className="clientOpinionBox w-[286px] h-[114px] bg-[#FFFFFF] ml-[149px] mt-[42px] rounded-[5px]">
+                        <div className="clientOpinionBox w-[286px] h-[114px]  bg-[#FFFFFF] ml-[149px] mt-[42px] rounded-[5px]">
                             <div className="flex flex-row self-end item-end justify-end">
                                 <div className="flex flex-row gap-[2px] mt-[20px] mr-[8px]">
                                     <img src="/star.png" alt="" className="stars" />
@@ -30,7 +36,7 @@ export function ClientsOpinion(){
                                 همکاری با ماوریکس یکی از بهترین تجربه‌های کاری تیم ما بود. دقت، ظرافت و پاسخگویی سریع از ویژگی‌های ماوریکس هست..
                             </p>
                         </div>
-                        <div className="clientOpinionBox w-[286px] h-[114px] bg-[#FFFFFF] mt-[42px] ml-[20px] rounded-[5px]">
+                        <div className="clientOpinionBox z-1000 w-[286px] h-[114px] bg-[#FFFFFF] mt-[42px] ml-[20px] rounded-[5px]">
                             <div className="flex flex-row self-end item-end justify-end">
                                 <div className="flex flex-row gap-[2px] mt-[20px] mr-[8px]">
                                     <img src="/star.png" alt="" className="stars" />
@@ -50,6 +56,7 @@ export function ClientsOpinion(){
                             </p>
                         </div>
                     </div>
+                    <img src="/PolygonLeft.png" alt="" className="Polygons2" />
                 </div>
                 <div className="rightOpinionBox flex self-end -top-[13.2rem]" ></div>
             </div>
